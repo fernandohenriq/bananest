222beb0f2a433524b0e94e70a8b8d4cb2866b2fd
Fernando Henrique <fernandohenriqsilva@gmail.com>
2024-11-12 22:25:54 -0300

    refactor: remove unnecessary interfaces

--- src/framework/interfaces/app-controller.abstract.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '../app-decorators';
-import { AppHttp } from './app-http.interface';
-
-@Controller('/')
-export abstract class AppController {
-  [key: string]: (http: AppHttp) => Promise<void>;
-}
